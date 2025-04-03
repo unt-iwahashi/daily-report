1a00f2134a12416046d7373251269198883dfad5
Iwahashi Aki <iwahashi@un-t.com>
2025-04-03 12:32:25 +0900

    add: ベースファイル作成

---
+++ src/blocks/InputArea.tsx
@@ -0,0 +1,3 @@
+export const InputArea = () => {
+  return <div>InputArea</div>;
+};
---
+++ src/blocks/ViewArea.tsx
@@ -0,0 +1,10 @@
+import { ReportTitle } from '@/components/Title/ReportTitle';
+
+export const ViewArea = () => {
+  return (
+    <>
+      <ReportTitle title="ViewArea" />
+      <div>ViewArea</div>
+    </>
+  );
+};
