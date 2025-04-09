278d5290fdb93d191fd1069f064afa4956d86233
Iwahashi Aki <iwahashi@un-t.com>
2025-04-09 15:32:03 +0900

    add: タイトル追加

--- src/blocks/ViewArea.tsx
+++ src/blocks/ViewArea.tsx
@@ -3,8 +3,11 @@ import { ReportTitle } from '@/components/Title/ReportTitle';
 export const ViewArea = () => {
   return (
     <>
-      <ReportTitle title="ViewArea" />
-      <div>ViewArea</div>
+      <ReportTitle title="今日行ったこと" />
+      <ReportTitle title="明日やること" />
+      <ReportTitle title="出退勤時間" />
+      <ReportTitle title="自宅作業（作業した場合のみ）" />
+      <ReportTitle title="その他" />
     </>
   );
 };
